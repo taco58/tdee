ef37a9851feec73a1f456059ccc4ad637e1b6095
kelvin <kelvinwu0002@gmail.com>
2026-08-12 20:21:31 -0700

    added robots.txt and sitemap

---
+++ src/app/robots.ts
@@ -0,0 +1,27 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = 'https://adaptdee.xyz'
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: [
+        '/dashboard',
+        '/login',
+        '/signup',
+        '/info-form',
+        '/check-email',
+        '/forgot-password',
+        '/reset-password',
+        '/api/',
+        '/auth/',
+        '/privacy',
+        '/terms',
+        '/error',
+      ],
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  }
+}
